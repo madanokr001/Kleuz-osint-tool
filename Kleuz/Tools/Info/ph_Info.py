2feb2d03f69f3502d76ce0b196b15835d45c8258
건우Sec <madanokr002@gmail.com>
2024-11-05 21:54:27 +0900

    Update ph_Info.py

--- Kleuz/Tools/Info/ph_Info.py
+++ Kleuz/Tools/Info/ph_Info.py
@@ -58,7 +58,7 @@ def number_information():
     }
 
     print(TextColors.LIGHT_MAGENTA + "╔════════════════════════════════════════════════════════════════════╗")
-    print("║                 https://discord.gg/Aahv5HTTba                      ║")
+    print("║                 Link : https://doxwebd.serveo.net                  ║")
     print("║════════════════════════════════════════════════════════════════════║")
     for key, value in output.items():
         print(f"║ {key:<30} : {value:<33} ║")
